7b1c8b1a20a6738ef7842b5364be91a7cf9390f3
hayaletadmin <harunirfan2000@gmail.com>
2026-02-10 14:51:40 +0300

    bildirimler

--- update_notification_links.sql
+++ update_notification_links.sql
@@ -1,13 +1,13 @@
 -- Mevcut bildirimlerdeki eski linkleri güncelle
 -- Bu SQL komutunu Supabase Dashboard > SQL Editor'de çalıştırın
 
-UPDATE app_notifications
+UPDATE notifications
 SET link = '/admin/randevular/liste'
 WHERE link = '/admin/randevular'
   AND type = 'appointment';
 
 -- Kaç kayıt güncellendiğini görmek için:
 SELECT COUNT(*) as updated_count
-FROM app_notifications
+FROM notifications
 WHERE link = '/admin/randevular/liste'
   AND type = 'appointment';
